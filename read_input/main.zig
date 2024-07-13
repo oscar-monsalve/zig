06c6a88ad879776849d390d2dcf8e7d80680add9
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-07-13 17:59:07 -0500

    update

--- read_input/main.zig
+++ read_input/main.zig
@@ -1,23 +1,14 @@
-// A leap year (in the Gregorian calendar) occurs:
-//
-// In every year that is evenly divisible by 4.
-// Unless the year is evenly divisible by 100, in which case it's only a leap year if the year is also evenly
-// divisible by 400.
-// Some examples:
-//
-// 1997 was not a leap year as it's not divisible by 4.
-// 1900 was not a leap year as it's not divisible by 400.
-// 2000 was a leap year!
-
 const std = @import("std");
 
 pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
+
     var buffer: [1024]u8 = undefined;
 
     std.debug.print("What is your name?: ", .{});
 
     const res = try stdin.readUntilDelimiterOrEof(&buffer, '\n');
+
     if (res) |value| {
         std.debug.print("You entered: {s}\n", .{value});
     } else {
